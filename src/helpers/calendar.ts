bdb7a9aa320f170e289fd6924d4ba2c2ec134918
Colin Luzio <blissful1980@gmail.com>
2020-08-08 10:34:52 +0100

    check if 31 days

--- src/helpers/calendar.ts
+++ src/helpers/calendar.ts
@@ -4,16 +4,25 @@ export const getPrevMonthDays = (date: Date) => {
     let month = date.getMonth()
     let startDay = `${month + 1} / 01 / ${date.getFullYear()}`
 
-    let prevMonth = new Date(date.getMonth() - 1)
+    let prevMonth = new Date()
+    prevMonth.setMonth(date.getMonth() - 1)
     let totalDays =
-        prevMonth.getMonth() == (0 || 2 || 4 || 6 || 7 || 9 || 11) ? 31 : 30
+        prevMonth.getMonth() == 0 ||
+        prevMonth.getMonth() == 2 ||
+        prevMonth.getMonth() == 4 ||
+        prevMonth.getMonth() == 6 ||
+        prevMonth.getMonth() == 7 ||
+        prevMonth.getMonth() == 9 ||
+        prevMonth.getMonth() == 11
+            ? 31
+            : 30
     totalDays =
         prevMonth.getMonth() !== 1
             ? totalDays
             : prevMonth.getFullYear() % 4 === 0
             ? 29
             : 28
-
+    console.log(prevMonth.getMonth())
     console.log(new Date(startDay).getDay())
     console.log(totalDays)
 }
