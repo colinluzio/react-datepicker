b6a0afb76c58593d7ec5f7eaa67a3481447d8fed
Colin Luzio <blissful1980@gmail.com>
2020-08-09 07:53:44 +0100

    add method to get total number of days

--- src/helpers/calendar.ts
+++ src/helpers/calendar.ts
@@ -1,28 +1,38 @@
 export const weekdays = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 
-export const getPrevMonthDays = (date: Date) => {
-    let month = date.getMonth()
-    let startDay = `${month + 1} / 01 / ${date.getFullYear()}`
-
-    let prevMonth = new Date()
-    prevMonth.setMonth(date.getMonth() - 1)
+const getTotalDays = (date: Date): number => {
     let totalDays =
-        prevMonth.getMonth() == 0 ||
-        prevMonth.getMonth() == 2 ||
-        prevMonth.getMonth() == 4 ||
-        prevMonth.getMonth() == 6 ||
-        prevMonth.getMonth() == 7 ||
-        prevMonth.getMonth() == 9 ||
-        prevMonth.getMonth() == 11
+        date.getMonth() == 0 ||
+        date.getMonth() == 2 ||
+        date.getMonth() == 4 ||
+        date.getMonth() == 6 ||
+        date.getMonth() == 7 ||
+        date.getMonth() == 9 ||
+        date.getMonth() == 11
             ? 31
             : 30
     totalDays =
-        prevMonth.getMonth() !== 1
+        date.getMonth() !== 1
             ? totalDays
-            : prevMonth.getFullYear() % 4 === 0
+            : date.getFullYear() % 4 === 0
             ? 29
             : 28
-    console.log(prevMonth.getMonth())
-    console.log(new Date(startDay).getDay())
-    console.log(totalDays)
+
+    return totalDays
+}
+
+export const getPrevMonthDays = (date: Date): Array<number> => {
+    let month = date.getMonth()
+    let startDay = `${month + 1} / 01 / ${date.getFullYear()}`
+
+    let totalDays = getTotalDays(new Date(date.getMonth() - 1))
+    let prevMonthDays = []
+
+    for (let x = 1; x < totalDays + 1; x++) {
+        prevMonthDays.push(x)
+    }
+    console.log(startDay)
+    console.log(prevMonthDays)
+
+    return prevMonthDays
 }
